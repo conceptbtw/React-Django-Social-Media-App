bd76ed0735d991a8246650a30c743e515ac6e4c4
Illia Movchko <illia.movchko.concept@gmail.com>
2024-03-24 08:09:54 +0000

    Install React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 export default function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
